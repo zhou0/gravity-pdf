54aaad8348a52a4c04e1f70c23b13cb247e1dd98
debbieellis7 <debbieellis7@gmail.com>
2019-08-12 19:50:41 +0800

    display "Download PDF" as an option on the Entry List page instead of View PDF when "Download" is selected

--- tests/e2e/global-settings/general/entry-view-field.test.js
+++ tests/e2e/global-settings/general/entry-view-field.test.js
@@ -7,17 +7,51 @@ const run = new General()
 fixture`General Tab - Entry View Field Test`
 
 test('should display Entry View field', async t => {
-  // Get selectors
-  const view = Selector('div').find('[class^="gfpdf_settings_default_action"][value="View"]')
-  const downlaod = Selector('div').find('[class^="gfpdf_settings_default_action"][value="Download"]')
-
   // Actions
   await run.navigateSettingsTab('gf_settings&subview=PDF&tab=general#')
 
   // Assertions
   await t
     .expect(fieldLabel('Entry View').exists).ok()
-    .expect(view.exists).ok()
-    .expect(downlaod.exists).ok()
+    .expect(run.viewOption.exists).ok()
+    .expect(run.downlaodOption.exists).ok()
     .expect(fieldDescription('Select the default action used when accessing a PDF from the Gravity Forms entries list page.', 'label').exists).ok()
+})
+
+test('should display "Download PDF" as an option on the Entry List page instead of View PDF when "Download" is selected', async t => {
+  // Selectors
+  const saveButton = Selector('div').find('[class^="button button-primary"][value="Save Changes"]')
+  const downloadPdfLink = Selector('a').withText('Download PDF')
+
+  // Actions
+  await run.navigateSettingsTab('gf_settings&subview=PDF&tab=general#')
+  await t
+    .click(run.downlaodOption)
+    .click(saveButton)
+  await run.navigateAddPdf('gf_edit_forms')
+
+  // Assertions
+  await t
+    .expect(downloadPdfLink.exists).ok()
+})
+
+test('should delete Gravity PDF templates from the list', async t => {
+  // Actions
+  await run.navigatePdfEntries('gf_edit_forms')
+  await t
+    .hover(run.settingsMenu)
+    .click(run.pdfLink)
+  let tempalte = await run.template.count
+  if (tempalte > 0) {
+    for (let i = 0; i < tempalte; i++) {
+      await t
+        .hover(run.template)
+        .click(run.deletePDF)
+        .click(run.confirmDelete)
+        .wait(2000)
+    }
+  }
+
+  // Assertions
+  await t.expect(run.template.count).eql(0)
 })
--- tests/e2e/page-model/global-settings/general/general.js
+++ tests/e2e/page-model/global-settings/general/general.js
@@ -8,12 +8,50 @@ class General {
     this.paperSizeSelectBox = Selector('div').find('[class^="chosen-container chosen-container-single"][id="gfpdf_settings_default_pdf_size__chosen"]')
     this.templateSelectBox = Selector('div').find('[class^="chosen-container chosen-container-single"][id="gfpdf_settings_default_template__chosen"]')
     this.templatePopupBox = Selector('div').find('[class^="container theme-wrap"]')
+    this.viewOption = Selector('div').find('[class^="gfpdf_settings_default_action"][value="View"]')
+    this.downlaodOption = Selector('div').find('[class^="gfpdf_settings_default_action"][value="Download"]')
+    this.formname = Selector('#the-list').find('tr').withText('Sample 2')
+    this.entries = Selector('#the-list').find('tr').withText('Sample 2').find('span').withText('Entries')
+    this.settingsMenu = Selector('.gf_form_toolbar_settings')
+    this.pdfLink = Selector('.gf_submenu').find('a').withText('PDF')
+    this.addNewPdf = Selector('#add-new-pdf')
+    this.pdfname = Selector('#gfpdf_settings\\[name\\]')
+    this.fileName = Selector('#gfpdf_settings\\[filename\\]')
+    this.addPdfButton = Selector('div').find('[class^="button-primary"][value="Add PDF"]')
+    this.list = Selector('.gf-locking ').withText('Sample 2')
+    this.template = Selector('.alternate')
+    this.deletePDF = Selector('.submitdelete')
+    this.confirmDelete = Selector('button').find('[class^="ui-button-text"]').withText('Delete')
   }
 
   async navigateSettingsTab (text) {
     await t
       .useRole(admin)
       .navigateTo(`${baseURL}/wp-admin/admin.php?page=${text}`)
+  }
+
+  async navigateAddPdf (text) {
+    await t
+      .navigateTo(`${baseURL}/wp-admin/admin.php?page=${text}`)
+      .hover(this.formname)
+      .click(this.entries)
+      .hover(this.settingsMenu)
+      .click(this.pdfLink)
+      .click(this.addNewPdf)
+      .typeText(this.pdfname, 'Test PDF Template', { paste: true })
+      .typeText(this.fileName, 'testpdftemplate', { paste: true })
+      .click(this.addPdfButton)
+      .navigateTo(`${baseURL}/wp-admin/admin.php?page=${text}`)
+      .hover(this.formname)
+      .click(this.entries)
+  }
+
+  async navigatePdfEntries (text) {
+    await t
+      .useRole(admin)
+      .navigateTo(`${baseURL}/wp-admin/admin.php?page=${text}`)
+      .hover(this.list)
+      .click(this.entries)
   }
 }
 
