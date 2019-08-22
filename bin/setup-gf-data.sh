2aab0adaebc0cdb519c911e3cd194b6828a5593f
Jake Jackson <jake@gravitypdf.com>
2019-08-22 16:10:23 +1000

    - Include Name / Email field in the forms
    - Preload PDF for Form 3 / 4

--- bin/setup-gf-data.sh
+++ bin/setup-gf-data.sh
@@ -9,6 +9,11 @@ echo -e $(status_message "Pre-load Gravity Forms data")
 
 for i in {1..5}
 do
-  FID=$(docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI gf form create "Sample $i" --form-json='{"fields":[{"type":"text", "id": 1, "label": "Text"}]}' --porcelain)
+  FID=$(docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI gf form create "Sample $i" --form-json='{"fields":[{"type":"text", "id": 1, "label": "Text"},{"type":"name", "id": 2, "label": "Name"},{"type":"email", "id": 3, "label": "Email"}]}' --porcelain)
   docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI gf entry create $FID --field_1=value --quiet
+
+  # Add PDF Configs
+  if [[ $i -eq 3  ||  $i -eq 4 ]]; then
+    docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI eval "GPDFAPI::add_pdf(${FID}, [ 'name' => 'Sample', 'template' => 'zadani', 'filename' => 'Sample' ]);"
+  fi
 done
