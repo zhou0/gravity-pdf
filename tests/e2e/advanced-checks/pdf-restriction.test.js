9779ff0be63f881839e85034a4ddc65a4cc8037a
debbieellis7 <debbieellis7@gmail.com>
2019-09-02 13:34:50 +0800

    Fixed testcafe user role issue

--- tests/e2e/advanced-checks/pdf-restriction.test.js
+++ tests/e2e/advanced-checks/pdf-restriction.test.js
@@ -1,4 +1,4 @@
-import { user, baseURL } from '../auth'
+import { baseURL } from '../auth'
 import ConfirmationShortcodes from '../page-model/advanced-checks/confirmation-shortcode'
 import PdfRestriction from '../page-model/advanced-checks/pdf-restriction'
 
@@ -6,19 +6,35 @@ const cs = new ConfirmationShortcodes()
 const run = new PdfRestriction()
 let pdfId
 
+const loginAsAdmin = async t => {
+  await t
+    .typeText('#user_login', 'admin', { paste: true })
+    .typeText('#user_pass', 'password', { paste: true })
+    .click('#wp-submit')
+}
+
+const loginAsUser = async t => {
+  await t
+    .typeText('#user_login', 'user', { paste: true })
+    .typeText('#user_pass', 'password', { paste: true })
+    .click('#wp-submit')
+}
+
 fixture`PDF Administrator & Non-Administrator - Restriction Test`
+  .page(`${baseURL}/wp-admin/admin.php?page=gf_edit_forms&view=settings&subview=pdf&id=4`)
 
 test('should throw an error when a non-administrator user try to access a PDF', async t => {
   // Actions
-  await cs.copyDownloadShortcode('gf_edit_forms&view=settings&subview=pdf&id=4')
+  await loginAsAdmin(t)
+  await t
+    .navigateTo(`${baseURL}/wp-admin/admin.php?page=gf_edit_forms&view=settings&subview=pdf&id=4`)
   pdfId = await cs.shortcodeField.value
   pdfId = pdfId.substring(30, 43)
   await t
     .hover(run.wpAdminBar)
     .click(run.logout)
-  await t
-    .useRole(user)
-    .navigateTo(`${baseURL}/pdf/${pdfId}/4`)
+  await loginAsUser(t)
+  await t.navigateTo(`${baseURL}/pdf/${pdfId}/4`)
 
   // Assertions
   await t.expect(run.errorMessage.exists).ok()
--- tests/e2e/auth.js
+++ tests/e2e/auth.js
@@ -7,15 +7,15 @@ export const baseURL = process.env.E2E_TESTING_URL
 export const admin = Role(`${baseURL}/wp-login.php`, async t => {
   await t
     .wait(100)
-    .typeText('#user_login', 'admin')
-    .typeText('#user_pass', 'password')
+    .typeText('#user_login', 'admin', { paste: true })
+    .typeText('#user_pass', 'password', { paste: true })
     .click('#wp-submit')
 })
 
 export const user = Role(`${baseURL}/wp-login.php`, async t => {
   await t
     .wait(100)
-    .typeText('#user_login', 'user')
-    .typeText('#user_pass', 'password')
+    .typeText('#user_login', 'user', { paste: true })
+    .typeText('#user_pass', 'password', { paste: true })
     .click('#wp-submit')
 })
