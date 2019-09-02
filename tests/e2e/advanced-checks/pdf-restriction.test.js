5a9a2b0d77c4f77913c47be88bf8bd31a1be5451
debbieellis7 <debbieellis7@gmail.com>
2019-09-02 12:14:06 +0800

    Refactored PDF Restriction Test

--- tests/e2e/advanced-checks/pdf-restriction.test.js
+++ tests/e2e/advanced-checks/pdf-restriction.test.js
@@ -1,4 +1,4 @@
-import { baseURL } from '../auth'
+import { user, baseURL } from '../auth'
 import ConfirmationShortcodes from '../page-model/advanced-checks/confirmation-shortcode'
 import PdfRestriction from '../page-model/advanced-checks/pdf-restriction'
 
@@ -6,9 +6,9 @@ const cs = new ConfirmationShortcodes()
 const run = new PdfRestriction()
 let pdfId
 
-fixture`PDF Logout User - Restriction Test`
+fixture`PDF Administrator & Non-Administrator - Restriction Test`
 
-test('should throw an error when a logout user try to access a PDF', async t => {
+test('should throw an error when a non-administrator user try to access a PDF', async t => {
   // Actions
   await cs.copyDownloadShortcode('gf_edit_forms&view=settings&subview=pdf&id=4')
   pdfId = await cs.shortcodeField.value
@@ -16,7 +16,17 @@ test('should throw an error when a logout user try to access a PDF', async t => 
   await t
     .hover(run.wpAdminBar)
     .click(run.logout)
+  await t
+    .useRole(user)
     .navigateTo(`${baseURL}/pdf/${pdfId}/4`)
+
+  // Assertions
+  await t.expect(run.errorMessage.exists).ok()
+})
+
+test('should throw an error when a logout user try to access a PDF', async t => {
+  // Actions
+  await t.navigateTo(`${baseURL}/pdf/${pdfId}/4`)
 
   // Assertions
   await t.expect(run.errorMessage.exists).ok()
