ea0aa76d70eeb600ca529cc993a330b92efb37a3
Jake Jackson <jake@gravitypdf.com>
2019-09-04 17:42:24 +1000

    s

--- bin/setup-plugins.sh
+++ bin/setup-plugins.sh
@@ -7,6 +7,10 @@
 # Setting up Gravity Forms
 echo -e $(status_message "Installing and configuring Gravity Forms")
 
+echo $UID
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER echo $UID
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI echo $UID
+
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI plugin install gravityformscli --activate --force
 
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI gf install --key=$GF_LICENSE --activate --force
