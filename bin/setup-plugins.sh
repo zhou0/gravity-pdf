8786daa35b60b1fa6f9e34cacaa68186492c4eeb
Jake Jackson <jake@gravitypdf.com>
2019-09-05 15:27:54 +1000

    add debugging

--- bin/setup-plugins.sh
+++ bin/setup-plugins.sh
@@ -7,12 +7,7 @@
 # Setting up Gravity Forms
 echo -e $(status_message "Installing and configuring Gravity Forms")
 
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI mkdir wp-content/plugins/test
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI touch wp-content/plugins/test/file
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI ls -la wp-content/plugins
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI ls -la wp-content/plugins/test
-
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI plugin install gravityformscli --activate --force
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI plugin install gravityformscli --activate --force --debug
 
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -u 33 --rm $CLI gf install --key=$GF_LICENSE --activate --force
 
