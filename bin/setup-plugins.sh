74749d29f397aa7c061d71ce04c0f8d02996c047
Jake Jackson <jake@gravitypdf.com>
2019-09-05 14:50:18 +1000

    Check to see if it's a WP engine problem or not

--- bin/setup-plugins.sh
+++ bin/setup-plugins.sh
@@ -7,6 +7,8 @@
 # Setting up Gravity Forms
 echo -e $(status_message "Installing and configuring Gravity Forms")
 
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI mkdir wp-content/plugins/test && touch wp-content/plugins/test/file && ls -la wp-content/plugins && ls -la wp-content/plugins/test
+
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI plugin install gravityformscli --activate --force
 
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CLI gf install --key=$GF_LICENSE --activate --force
